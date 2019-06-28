5a843df97012baae15d8e8c25d97152521e3a971
Daniel Borowski <danielborowski@Daniels-MacBook-Pro-5.local>
2019-06-28 10:45:24 -0400

    test

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ sys.path.append(os.path.join(conf.paths.projectroot, conf.paths.buildsystem, 'sp
 needs_sphinx = '1.0'
 
 extensions = [ 
-    'sphinx.ext.extlinks',
+    'sphinx.ext.extlinks', 
     'sphinx.ext.todo',
     'mongodb',  
     'directives',
