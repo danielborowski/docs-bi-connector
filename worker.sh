1b6a3549dae1d450319f948bc29377b095923369
Daniel Borowski <danielborowski@Daniels-MacBook-Pro-5.local>
2019-09-09 15:26:59 -0400

    test

--- worker.sh
+++ worker.sh
@@ -14,9 +14,9 @@ cd $repoDir/snooty
 
 # create env variable for build
 touch .env.production
-echo 'GATSBY_SITE="cloud"' >> .env.production
-echo 'PARSER_USER="sophstad"' >> .env.production
-echo 'PARSER_BRANCH="master"' >> .env.production
+echo 'GATSBY_SITE=bi-connector' >> .env.production
+echo 'PARSER_USER=sophstad' >> .env.production
+echo 'PARSER_BRANCH=master' >> .env.production
 
 # begin build
 echo "------ SNOOTY CONTENTS ------"
