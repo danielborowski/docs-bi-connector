cf3a09b289e0c8d9c43d40560ffa3f0644c3350a
Daniel Borowski <danielborowski@Daniels-MacBook-Pro-5.local>
2019-09-09 14:47:31 -0400

    test

--- worker.sh
+++ worker.sh
@@ -2,28 +2,26 @@
 #repoDir=$(pwd)
 #snooty build $pwd || exit 0
 #npm run build
- 
-export GATSBY_SITE="guides"
-export PARSER_USER="sophstad"
-export PARSER_BRANCH="master"
-
-GATSBY_SITE="guides"
 
 repoDir=$(pwd)
 cd ..
 rootDir=$(pwd)
 snootyDir=$rootDir/snooty
 
+cp $repoDir/snooty/.env.production $repoDir
+cp $repoDir/snooty/Makefile $repoDir
+
+echo 'GATSBY_SITE="guides"' >> $repoDir/.env.production
+echo 'PARSER_USER="sophstad"' >> $repoDir/.env.production
+echo 'PARSER_BRANCH="master"' >> $repoDir/.env.production
+
 cp -r $snootyDir $repoDir
 cd $repoDir
 cd snooty
-npm run build 
+npm run build
 echo "------"
 ls
 echo "------"
-printenv
-cp $repoDir/snooty/.env.production $repoDir
-cp $repoDir/snooty/Makefile $repoDir
 
 #GIT_BRANCH=blabla
 #mkdir -p $repoDir/build/$GIT_BRANCH/html
